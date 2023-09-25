624ad694ee66dbcefaccabeb2ccffc7ea4c2dab4
hitsan <soledewa2828@gmail.com>
2023-09-25 19:38:30 +0900

    middle

--- calclator/src/test/scala/LexerSpec.scala
+++ calclator/src/test/scala/LexerSpec.scala
@@ -1,20 +1,20 @@
 class LexerSpec extends munit.FunSuite {
-import Model.Token._
-import Model.Token
-import Lexer._
+  import Model.Token._
+  import Model.Token
+  import Lexer._
 
-val n1 =  Num(1)
-val n2 =  Num(2)
-val n3 =  Num(3)
-val n4 =  Num(4)
-val n11 = Num(11)
-val n123 = Num(123)
-val a =  Add
-val s =  Sub
-val m =  Mul
-val d =  Div
+  val n1 = Num(1)
+  val n2 = Num(2)
+  val n3 = Num(3)
+  val n4 = Num(4)
+  val n11 = Num(11)
+  val n123 = Num(123)
+  val a = Add
+  val s = Sub
+  val m = Mul
+  val d = Div
 
-test("determine test") {
+  test("determine test") {
     var tok = determine('+')
     assertEquals(tok.get, a)
     tok = determine('-')
@@ -31,23 +31,23 @@ test("determine test") {
     assertEquals(tok.get, n3)
     tok = determine(' ')
     assertEquals(tok, None)
-}
+  }
 
-test("Lexer test") {
+  test("Lexer test") {
     (lex("1 + 2") zip List(n1, a, n2))
-        .foreach(r => assertEquals(r._1,r._2))
+      .foreach(r => assertEquals(r._1, r._2))
     (lex("2 - 1") zip List(n2, s, n1))
-        .foreach(r => assertEquals(r._1, r._2))
+      .foreach(r => assertEquals(r._1, r._2))
     (lex("3 * 4") zip List(n3, m, n4))
-        .foreach(r => assertEquals(r._1, r._2))
+      .foreach(r => assertEquals(r._1, r._2))
     (lex("4 / 2") zip List(n4, d, n2))
-        .foreach(r => assertEquals(r._1, r._2))
+      .foreach(r => assertEquals(r._1, r._2))
     (lex("4 + 2 - 1") zip List(n4, a, n2, s, n1))
-        .foreach(r => assertEquals(r._1, r._2))
+      .foreach(r => assertEquals(r._1, r._2))
     (lex("11") zip List(n11))
-        .foreach(r => assertEquals(r._1, r._2))
+      .foreach(r => assertEquals(r._1, r._2))
     (lex("123 + 11") zip List(n123, a, n11))
-        .foreach(r => assertEquals(r._1, r._2))
-}
+      .foreach(r => assertEquals(r._1, r._2))
+  }
 
 }
