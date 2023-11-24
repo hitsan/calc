ed57463b44a276ac507eacc0e84858491f7b6f66
hitsan <soledewa2828@gmail.com>
2023-11-24 18:01:26 +0000

    change expression to parser

--- calclator/src/main/scala/parser/Eval.scala
+++ calclator/src/main/scala/parser/Eval.scala
@@ -3,9 +3,13 @@ package parser
 object Eval {
   import Node._
 
-  extension (result: Option[PResult[Node]])
+  extension (result: Either[ParseError, Node])
     def eval: Int =
-      result.flatMap(r => Some(calc(r.token))).getOrElse(0)
+      result.map(node => calc(node)).getOrElse(0)
+
+  extension (result: Either[ParseError, Node])
+    def evalBool: Boolean =
+      result.map(node => calcBool(node)).getOrElse(false)
 
   def calc(node: Node): Int = {
     node match {
@@ -16,6 +20,20 @@ object Eval {
       case IntNum(n)     => n
       case Negative(n)   => -calc(n)
       case _             => 0
+    }
+  }
+
+  def calcBool(node: Node): Boolean = {
+    node match {
+      case Equals(lhs, rhs)       => calc(lhs) == calc(rhs)
+      case NotEquals(lhs, rhs)    => calc(lhs) != calc(rhs)
+      case Less(lhs, rhs)         => calc(lhs) < calc(rhs)
+      case LessEqual(lhs, rhs)    => calc(lhs) <= calc(rhs)
+      case Greater(lhs, rhs)      => calc(lhs) > calc(rhs)
+      case GreaterEqual(lhs, rhs) => calc(lhs) >= calc(rhs)
+      case Bool(value)            => value
+      case Bang(rhs)              => !calcBool(rhs)
+      case _                      => false
     }
   }
 
--- calclator/src/test/scala/EvalSpec.scala
+++ calclator/src/test/scala/EvalSpec.scala
@@ -2,36 +2,79 @@ import parser.PResult
 class EvalSpec extends munit.FunSuite {
   import parser.Expression._
   import parser.Eval._
+  import parser.Parser._
 
   test("add") {
-    assertEquals(expression("1+2").eval, 1 + 2)
-    assertEquals(expression("2 + 5").eval, 2 + 5)
+    assertEquals(parse("1+2").eval, 1 + 2)
+    assertEquals(parse("2 + 5").eval, 2 + 5)
   }
 
   test("sub") {
-    assertEquals(expression("2-2").eval, 2 - 2)
-    assertEquals(expression("3-2").eval, 3 - 2)
-    assertEquals(expression("2 - 5").eval, 2 - 5)
+    assertEquals(parse("2-2").eval, 2 - 2)
+    assertEquals(parse("3-2").eval, 3 - 2)
+    assertEquals(parse("2 - 5").eval, 2 - 5)
   }
 
   test("mul") {
-    assertEquals(expression("2*2").eval, 2 * 2)
-    assertEquals(expression("2 * 5").eval, 2 * 5)
+    assertEquals(parse("2*2").eval, 2 * 2)
+    assertEquals(parse("2 * 5").eval, 2 * 5)
   }
   test("div") {
-    assertEquals(expression("2/2").eval, 2 / 2)
-    assertEquals(expression("2 / 5").eval, 2 / 5)
-    assertEquals(expression("6 / 2").eval, 6 / 2)
+    assertEquals(parse("2/2").eval, 2 / 2)
+    assertEquals(parse("2 / 5").eval, 2 / 5)
+    assertEquals(parse("6 / 2").eval, 6 / 2)
   }
 
   test("exprssion") {
-    assertEquals(expression("1+2/2*5").eval, 1 + 2 / 2 * 5)
-    assertEquals(expression("22 / 5+ 5*6+3").eval, 22 / 5 + 5 * 6 + 3)
-    assertEquals(expression("22 - (5+ 5)*6+3").eval, 22 - (5 + 5) * 6 + 3)
+    assertEquals(parse("1+2/2*5").eval, 1 + 2 / 2 * 5)
+    assertEquals(parse("22 / 5+ 5*6+3").eval, 22 / 5 + 5 * 6 + 3)
+    assertEquals(parse("22 - (5+ 5)*6+3").eval, 22 - (5 + 5) * 6 + 3)
     assertEquals(
-      expression("22 * (5 + (4 + 5)*6)+3").eval,
+      parse("22 * (5 + (4 + 5)*6)+3").eval,
       22 * (5 + (4 + 5) * 6) + 3
     )
-    assertEquals(expression("1---2").eval, 1-(-(-2)))
+    assertEquals(parse("1---2").eval, 1 - (-(-2)))
   }
+
+  test("equals") {
+    assertEquals(parse("1 == 1").evalBool, true)
+    assertEquals(parse("2 == 3").evalBool, false)
+  }
+
+  test("notEquals") {
+    assertEquals(parse("1 != 1").evalBool, false)
+    assertEquals(parse("2 != 3").evalBool, true)
+  }
+
+  test("less") {
+    assertEquals(parse("1 < 2").evalBool, true)
+    assertEquals(parse("2 < 1").evalBool, false)
+  }
+
+  test("lessEqual") {
+    assertEquals(parse("1 <= 2").evalBool, true)
+    assertEquals(parse("2 <= 2").evalBool, true)
+    assertEquals(parse("3 <= 2").evalBool, false)
+  }
+
+  test("greater") {
+    assertEquals(parse("2 > 1").evalBool, true)
+    assertEquals(parse("1 > 2").evalBool, false)
+  }
+
+  test("greaterEqual") {
+    assertEquals(parse("2 >= 1").evalBool, true)
+    assertEquals(parse("2 >= 2").evalBool, true)
+    assertEquals(parse("2 >= 3").evalBool, false)
+  }
+
+  test("bool") {
+    assertEquals(parse("true").evalBool, true)
+    assertEquals(parse("false").evalBool, false)
+  }
+
+  // test("bang") {
+  //   assertEquals(parse("!true").evalBool, false)
+  //   assertEquals(parse("!false").evalBool, true)
+  // }
 }
