8c2131baaa49eb49109d5b97738c777959f823c6
hitsan <soledewa2828@gmail.com>
2023-12-24 18:23:57 +0000

    Add map

--- calclator/src/main/scala/environment/Environment.scala
+++ calclator/src/main/scala/environment/Environment.scala
@@ -1,5 +1,9 @@
 package environment
 
 object Environment {
+  import parser.Node
+  import scala.collection.mutable.Map
 
+  private val values = Map.empty[String, Node]
+  def setValue(name: String, value: Node): Unit = values += (name -> value)
 }
